bd1629b0fe3979438c7694e80830d0602e3fde69
Seth Stalley <sethstalley@gmail.com>
2015-06-06 17:37:52 -0600

    add bootstrap

--- pythonClient/main.py
+++ pythonClient/main.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 from sql import SQLConnection
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Db connected"
+    return render_template('userSelect.html')
 
 if __name__ == "__main__":
     sqlCon = SQLConnection("autionDB", 'user', "123")
