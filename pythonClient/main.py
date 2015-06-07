2bb1edf32cb0947e52a42f418797b38ee8c037b9
Seth Stalley <sethstalley@gmail.com>
2015-06-07 00:47:45 -0600

    added more page layouts

--- pythonClient/main.py
+++ pythonClient/main.py
@@ -13,8 +13,32 @@ def login():
 def newListing():
     return render_template('newListing.html')
 
+
+#Agent stuff
+@app.route("/newAgent")
+def newAgent():
+    return render_template('newAgent.html')
+
+@app.route("/modifyAgent")
+def modifyAgent():
+    return render_template('modifyAgent.html')
+
+@app.route("/reactivateAgent")
+def reactivateAgent():
+    return render_template('reactivateAgent.html')
+
+@app.route("/suspendAgent")
+def suspendAgent():
+    return render_template('suspendAgent.html')
+
+#Listing stuff
+@app.route("/showListings")
+def showListings():
+    return render_template('showListings.html')
+
+
 if __name__ == "__main__":
     sqlCon = SQLConnection("autionDB", 'user', "123")
-    con = sqlCon.connect()
+    #con = sqlCon.connect()
     app.debug = True    #auto refresh
     app.run()
